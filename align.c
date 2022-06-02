10dd5a0469e4d2903f953fd587b6ceb5cc175cd5
Axel Scheepers <no@spam.please>
2022-06-02 17:13:15 +0200

    continuation lines might be better double indented

--- align.c
+++ align.c
@@ -139,7 +139,7 @@ int main(int argc, char **argv) {
 
         if (line->rest != NULL) {
             printf("%-*s%c%s\n", (int) max_first_length,
-                line->first, delimiter, line->rest);
+                    line->first, delimiter, line->rest);
             free(line->rest);
         } else {
             printf("%s\n", line->first);
