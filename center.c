3e09166796e6cc99f3b63151a3a1642de849a607
Axel Scheepers <no@spam.please>
2022-06-13 20:58:23 +0200

    style

--- center.c
+++ center.c
@@ -5,23 +5,24 @@
 #define BUFFER_SIZE 1024
 static char line[BUFFER_SIZE];
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     int width = 0;
 
-    if (argc > 1) {
+    if (argc > 1)
+    {
         width = atoi(argv[1]);
     }
 
-    if (width < 1) {
+    if (width < 1)
+    {
         width = 80;
     }
 
-    while (fgets(line, sizeof(line), stdin) != NULL) {
-        size_t line_length;
-        size_t length;
-
-        line_length = strlen(line);
-        length = (width - line_length) / 2;
+    while (fgets(line, sizeof(line), stdin) != NULL)
+    {
+        size_t line_length = strlen(line);
+        size_t length      = (width - line_length) / 2;
 
         printf("%*s", (int) (length + line_length), line);
     }
