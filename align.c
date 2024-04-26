c529835f3843b655dc844b233a30c0225e896640
Axel Scheepers <no@spam.please>
2024-04-26 20:14:03 +0200

    rework messy code

--- align.c
+++ align.c
@@ -2,164 +2,169 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
 #include <err.h>
 
-static void *xcalloc(size_t nmemb, size_t size)
+#define TABSIZE (8)
+
+static void *
+xmalloc(size_t size)
 {
-    void *ptr;
+	void *ptr;
 
-    ptr = calloc(nmemb, size);
-    if (ptr == NULL) {
-        errx(1, "calloc: out of memory.");
-    }
+	ptr = malloc(size);
+	if (ptr == NULL)
+		errx(1, "malloc: out of memory.");
 
-    return ptr;
+	return ptr;
 }
 
-static void *xrealloc(void *ptr, size_t size)
+static void *
+xrealloc(void *ptr, size_t size)
 {
-    void *new_ptr;
+	void *new_ptr;
 
-    new_ptr = realloc(ptr, size);
-    if (new_ptr == NULL) {
-        errx(1, "realloc: out of memory.");
-    }
+	new_ptr = realloc(ptr, size);
+	if (new_ptr == NULL)
+		errx(1, "realloc: out of memory.");
 
-    return new_ptr;
+	return new_ptr;
 }
 
-static char *xstrdup(const char *str)
+static char *
+xstrdup(const char *str)
 {
-    char *str_copy;
-    size_t size;
+	char *dup;
+	size_t size;
 
-    if (str == NULL) {
-        return NULL;
-    }
+	if (str == NULL)
+		return NULL;
 
-    size = strlen(str) + 1;
-    str_copy = xcalloc(1, size);
-    memcpy(str_copy, str, size);
+	size = strlen(str) + 1;
+	dup = xmalloc(size);
+	memcpy(dup, str, size);
 
-    return str_copy;
+	return dup;
 }
 
-/*
- * Aligned lines are split into a first and rest part by
- * a delimiter. The length of the first part is used to
- * calculate the alignment.
- */
-struct line {
-    char *first;
-    char *rest;
-    size_t first_length;
+struct string {
+	char *txt;
+	size_t len;
 };
 
-static struct line *get_line(FILE *input_file, int delimiter)
+static struct string *
+new_string(void)
 {
-    struct line *result = NULL;
-    char *delimiter_position = NULL;
-    char *line = NULL;
-    size_t line_length = 0;
-    size_t max_line_length = 0;
-    int ch;
+	struct string *str;
 
-    while ((ch = fgetc(input_file)) != EOF && ch != '\n') {
-        if (line_length >= max_line_length) {
-            line = xrealloc(line, max_line_length + 32);
-            max_line_length += 32;
-        }
+	str = xmalloc(sizeof(*str));
+	str->txt = NULL;
+	str->len = 0;
 
-        line[line_length++] = ch;
-    }
-
-    /*
-     * We should be able to read empty lines so
-     * only return NULL if at end of file.
-     */
-    if (line == NULL && ch == EOF) {
-        return NULL;
-    }
-
-    line = xrealloc(line, line_length + 1);
-    line[line_length] = '\0';
-
-    result = xcalloc(1, sizeof(*result));
-
-    /*
-     * Split the line into a first and rest part.  If no delimiter is
-     * found first will contain the full line (which may be the empty
-     * string).
-     */
-    delimiter_position = strchr(line, delimiter);
-
-    if (delimiter_position != NULL) {
-        char *rest = delimiter_position + 1;
-
-        *delimiter_position = '\0';
-
-        if (*rest != '\0') {
-            result->rest = xstrdup(rest);
-        }
-    }
-
-    result->first = xstrdup(line);
-    result->first_length = strlen(line);
-    free(line);
-
-    return result;
+	return str;
 }
 
-int main(int argc, char **argv)
+static void
+free_string(struct string *str)
 {
-    struct line *line = NULL;
-    struct line **lines = NULL;
-    size_t lines_count = 0;
-    size_t max_lines = 0;
-    size_t max_first_length = 0;
-    int delimiter = ' ';
-    size_t i;
-
-    if (argc == 2) {
-        delimiter = argv[1][0];
-    }
-
-    while ((line = get_line(stdin, delimiter)) != NULL) {
-        if (lines_count >= max_lines) {
-            size_t new_max = max_lines + 32;
-
-            lines = xrealloc(lines, sizeof(*lines) * new_max);
-            max_lines = new_max;
-        }
-
-        /* Only align and get the max length if we have two parts. */
-        if (line->rest != NULL && line->first_length > max_first_length) {
-            max_first_length = line->first_length;
-        }
-
-        lines[lines_count++] = line;
-    }
-
-    lines = xrealloc(lines, sizeof(*lines) * lines_count);
-
-    for (i = 0; i < lines_count; ++i) {
-        struct line *line = lines[i];
-
-        if (line->rest != NULL) {
-            printf("%-*s%c%s\n", (int) max_first_length, line->first,
-                   delimiter, line->rest);
-            free(line->rest);
-        } else {
-            printf("%s\n", line->first);
-        }
-
-        free(line->first);
-        free(line);
-    }
-
-    free(lines);
-
-    return 0;
+	free(str->txt);
+	free(str);
 }
 
+static struct string *
+read_string(FILE *fp)
+{
+	struct string *str;
+	size_t cap;
+	int ch;
+
+	str = new_string();
+	cap = 0;
+
+	while ((ch = fgetc(fp)) != EOF && ch != '\n') {
+		if (str->len >= cap) {
+			str->txt = xrealloc(str->txt, cap + 32);
+			cap += 32;
+		}
+		str->txt[str->len++] = ch;
+	}
+
+	if (str->txt == NULL && ch != '\n') {
+		free_string(str);
+		return NULL;
+	}
+
+	str->txt = xrealloc(str->txt, str->len + 1);
+	str->txt[str->len] = '\0';
+
+	return str;
+}
+
+static void
+write_string(const struct string *str, FILE *fp)
+{
+	fwrite(str->txt, str->len, 1, fp);
+	fputc('\n', fp);
+}
+
+static size_t
+int
+main(int argc, char **argv)
+{
+	struct string **lines = NULL;
+	size_t nlines = 0;
+	size_t cap = 0;
+
+	struct string *line;
+	size_t maxlen = 0;
+
+	int delimiter = ' ';
+	size_t i;
+	char *p;
+
+	if (argc == 2)
+		delimiter = argv[1][0];
+
+	/* Read all the things! */
+	while ((line = read_string(stdin)) != NULL) {
+		if (nlines >= cap) {
+			lines = xrealloc(lines, sizeof(*lines) * (cap + 32));
+			cap += 32;
+		}
+		lines[nlines++] = line;
+	}
+	lines = xrealloc(lines, sizeof(*lines) * nlines);
+
+	/* Find the maximum length of a string before the delimiter. */
+	for (i = 0; i < nlines; ++i) {
+		line = lines[i];
+		p = strchr(line->txt, delimiter);
+		if (p != NULL) {
+			size_t len;
+
+			*p = '\0';
+			len = strlen(line->txt);
+			if (len > maxlen)
+				maxlen = len;
+			*p = delimiter;
+		}
+	}
+
+	/* Align strings which have a delimiter. */
+	for (i = 0; i < nlines; ++i) {
+		line = lines[i];
+		p = strchr(line->txt, delimiter);
+		if (p != NULL && p[1] != '\n') {
+			*p = '\0';
+			printf("%-*s%c%s\n", (int)maxlen,
+			    line->txt, delimiter, &p[1]);
+			*p = delimiter;
+		} else
+			write_string(line, stdout);
+	}
+
+	for (i = 0; i < nlines; ++i)
+		free_string(lines[i]);
+	free(lines);
+
+	return 0;
+}
