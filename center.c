1aeabca2ce488ca7fb8499c241b4deab8d337870
Axel Scheepers <no@spam.please>
2024-08-04 22:26:05 +0200

    use unix style

--- center.c
+++ center.c
@@ -4,26 +4,24 @@
 
 static char line[1024];
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
-    int width = 0;
+	int width = 0;
 
-    if (argc > 1) {
-        width = atoi(argv[1]);
-    }
+	if (argc > 1)
+		width = atoi(argv[1]);
 
-    if (width < 1) {
-        width = 80;
-    }
+	if (width < 1)
+		width = 80;
 
-    while (fgets(line, sizeof(line), stdin) != NULL) {
-        size_t length;
-        size_t offset;
+	while (fgets(line, sizeof(line), stdin) != NULL) {
+		size_t len, offset;
 
-        length = strlen(line);
-        offset = (width - length) / 2;
-        printf("%*s", (int) (offset + length), line);
-    }
+		len = strlen(line);
+		offset = (width - len) / 2;
+		printf("%*s", (int)(offset + len), line);
+	}
 
-    return 0;
+	return 0;
 }
